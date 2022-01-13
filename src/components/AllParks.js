7f32e7c1fd9d064d3dc3a817da18e04e327c0caf
hey-chan <liumatthew8@gmail.com>
2022-01-13 23:56:33 +1100

    change root page to show card deck view for all parks

--- src/components/AllParks.js
+++ src/components/AllParks.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { APark } from './APark'
-import { CardDeck } from '../styled-components'
+import { CardDeck } from '../styled-components' // Uncomment this and change 
+import { ParkDescription } from './ParkDescription'
 
 // DEALS WITH RENDERING ALL DATA BY MAPPING OVER IT
 const AllParks = (props) => {
@@ -17,7 +17,7 @@ const AllParks = (props) => {
       (<CardDeck>
         {/* For each post, we will return APark element */}
         {/* AS we are mapping over an array, each APark will need a key */}
-        {parks.sort((a,b)=> b.updated_at - a.updated_at).map(park => (<APark key={park.id} park={park}/>) )}
+        {parks.sort((a,b)=> b.updated_at - a.updated_at).map(park => (<ParkDescription key={park.id} park={park}/>) )}
       </CardDeck>)
       }
 
--- src/components/ParkDescription.js
+++ src/components/ParkDescription.js
@@ -4,15 +4,15 @@ import { Card } from "../styled-components";
 
 // Will behave to APark.js
 
-export const ParkDescription = () => {
+export const ParkDescription = (props) => {
   const {park} = props;
 
 
   return (
     <Card>
-      <h4 style={{fontWeight: "bold"}}>{park.name}</h4>
-      <p>address: {park.address.number ? park.address.number : null} {park.address.street}, {park.address.suburb}, {park.address.postcode}</p>
-      <p>category: {park.category.name}</p>
+      <h4>Park: {park.park_name}{park.park_icon}</h4>
+      <p>address: {park.address_id }</p>
+      <p>category: {park.category_id}</p>
       <p>coordinates: {park.latitude} {park.longitude}</p>
     </Card>
   )
